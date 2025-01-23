1fd3b0c404f68298e0b8448add461e79194e031f
Denis.Kitrish <denis.kitrish@outlook.com>
2025-01-23 15:46:59 +0300

    10. Сканирование файловой системы [#106929]

---
+++ src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SearchTest {
+
+	@Test
+	void testSearchForTxt() {
+		List<Path> expected = List.of(
+				Path.of(".\\src\\main\\java\\ru\\job4j\\io\\search_resources\\one.txt"),
+				Path.of(".\\src\\main\\java\\ru\\job4j\\io\\search_resources\\three.txt")
+		);
+		Path start = Path.of(".");
+		List<Path> actual = Search.search(start, path -> path.toFile().getName().endsWith(".txt"));
+		assertThat(actual).isEqualTo(expected);
+	}
+}
