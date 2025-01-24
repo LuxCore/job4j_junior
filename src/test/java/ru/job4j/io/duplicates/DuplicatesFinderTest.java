b4245a0cb952e31c40783990d0ce9262b6310f2c
Denis.Kitrish <denis.kitrish@outlook.com>
2025-01-24 16:50:02 +0300

    11. Поиск дубликатов [#315066 #529864].
    Внесены изменения в тест: порядок элементов списков в картах теперь не влияет на сравнение.

--- src/test/java/ru/job4j/io/duplicates/DuplicatesFinderTest.java
+++ src/test/java/ru/job4j/io/duplicates/DuplicatesFinderTest.java
@@ -6,7 +6,8 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class DuplicatesFinderTest {
 
@@ -17,11 +18,19 @@ class DuplicatesFinderTest {
 		Map<FileProperty, List<Path>> actual = df.getDuplicateFiles(start);
 		Map<FileProperty, List<Path>> expected = Map.of(
 				new FileProperty("one.txt", 32), List.of(
-						Path.of("./src/main/java/ru/job4j/io/search_resources/a/one.txt"),
-						Path.of("./src/main/java/ru/job4j/io/search_resources/one.txt")),
+						Path.of("./src/main/java/ru/job4j/io/search_resources/one.txt"),
+						Path.of("./src/main/java/ru/job4j/io/search_resources/a/one.txt")
+				),
 				new FileProperty("three.txt", 0), List.of(
-						Path.of("./src/main/java/ru/job4j/io/search_resources/b/three.txt"),
-						Path.of("./src/main/java/ru/job4j/io/search_resources/three.txt")));
-		assertThat(actual).isEqualTo(expected);
+						Path.of("./src/main/java/ru/job4j/io/search_resources/three.txt"),
+						Path.of("./src/main/java/ru/job4j/io/search_resources/b/three.txt")
+				)
+		);
+
+		assertThat(actual).hasSameSizeAs(expected);
+		assertThat(actual.keySet()).isEqualTo(expected.keySet());
+		actual.forEach((key, value) ->
+				assertThat(value).containsExactlyInAnyOrderElementsOf(expected.get(key))
+		);
 	}
 }
