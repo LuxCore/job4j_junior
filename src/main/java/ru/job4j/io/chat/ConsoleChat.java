bf50e0e4f6a0dc4cb7f3e5b39e8ad0139457946f
Denis.Kitrish <denis.kitrish@outlook.com>
2025-01-30 23:16:20 +0300

    16. Кодировка [#862].

---
+++ src/main/java/ru/job4j/io/chat/ConsoleChat.java
@@ -0,0 +1,97 @@
+package ru.job4j.io.chat;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleChat {
+	private static final String OUT = "завершить";
+	private static final String STOP = "стоп";
+	private static final String CONTINUE = "продолжить";
+	private static final String IAM = "Я";
+	private static final String BOT = "Бот";
+	private final String path;
+	private final String botAnswers;
+
+	public ConsoleChat(String path, String botAnswers) {
+		this.path = path;
+		this.botAnswers = botAnswers;
+	}
+
+	public void run() {
+		Path outFile = Path.of(this.path);
+		createFile(outFile);
+		List<String> phrases = readPhrases();
+		List<String> chat = new LinkedList<>();
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		String botAnswer, myAnswer;
+		boolean exitApp = false, isBotStop = false;
+		while (!exitApp) {
+			if (!isBotStop) {
+				botAnswer = phrases.get(random.nextInt(phrases.size()));
+				System.out.println(BOT + ": " + botAnswer);
+				chat.add(BOT + ": " + botAnswer);
+			}
+			System.out.print(IAM + ": ");
+			myAnswer = scanner.nextLine();
+			exitApp = myAnswer.equals(OUT);
+			chat.add(IAM + ": " + myAnswer);
+			if (myAnswer.equals(STOP)) {
+				isBotStop = true;
+			} else if (myAnswer.equals(CONTINUE)) {
+				isBotStop = false;
+			}
+		}
+		saveLog(chat);
+	}
+
+	private static void createFile(Path outFile) {
+		try {
+			if (!Files.exists(outFile)) {
+				if (!Files.exists(outFile.getParent())) {
+					Files.createDirectories(outFile.getParent());
+				}
+				Files.createFile(outFile);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private List<String> readPhrases() {
+		List<String> phrases = new ArrayList<>();
+		try (BufferedReader reader = Files.newBufferedReader(Path.of(this.botAnswers))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				phrases.add(line);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return phrases;
+	}
+
+	private void saveLog(List<String> log) {
+		try (BufferedWriter writer = Files.newBufferedWriter(Path.of(this.path), StandardOpenOption.APPEND)) {
+			for (String line : log) {
+				writer.write(line + System.lineSeparator());
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		ConsoleChat consoleChat = new ConsoleChat("./data/chat/me-bot.log", "./data/chat/bot-answers.txt");
+		consoleChat.run();
+	}
+}
