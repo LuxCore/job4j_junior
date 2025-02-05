1764c50a3d682b2cd1bdf1995488f8cc4e6a2554
Denis.Kitrish <denis.kitrish@outlook.com>
2025-02-05 19:50:19 +0300

    0. JDBC [#6863].

---
+++ src/main/java/ru/job4j/sql_jdbc/jdbc/ConnectionDemo.java
@@ -0,0 +1,30 @@
+package ru.job4j.sql_jdbc.jdbc;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionDemo {
+	private static final String APP_PROPERTIES = "src/main/resources/app.properties";
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Properties properties = new Properties();
+		try (FileReader reader = new FileReader(APP_PROPERTIES)) {
+			properties.load(reader);
+		} catch (IOException e) {
+			throw new RuntimeException("Файл '%s' не найден.".formatted(APP_PROPERTIES));
+		}
+		String dbUrl = properties.getProperty("db.url");
+		String dbLogin = properties.getProperty("db.login");
+		String dbPassword = properties.getProperty("db.password");
+		try (Connection connection = DriverManager.getConnection(dbUrl, dbLogin, dbPassword)) {
+			DatabaseMetaData dbMetaData = connection.getMetaData();
+			System.out.println(dbMetaData.getUserName());
+			System.out.println(dbMetaData.getURL());
+		}
+	}
+}
